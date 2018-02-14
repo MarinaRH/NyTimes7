64ac5a5a80f171c020ddd8fbc819a5728f9dc068
MarinaRH <l_marina1717@hotmail.com>
2018-02-14 02:56:15 -0500

    agregando elementos a mostrar

--- public/js/app.js
+++ public/js/app.js
@@ -17,8 +17,7 @@ function getNews() {
     if (XHR.readyState === 4 && XHR.status === 200) {
       const data = JSON.parse(this.responseText);
       const response = data.response.docs;
-      XHR.onload = addNews(response); // función de ejecución
-      XHR.onerror = handleError; // funcion de error
+      XHR.onload = addNews(response);
     }
   };
   XHR.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`);
@@ -31,10 +30,10 @@ function handleError(){
 
 function addNews(response){
   if (response.length > 0) {
-    response.forEach(function(element) {
+    response.forEach(function(element,i) {
       if (element.document_type === 'article') {
-        const noticia = `<div class=" container"><div class = "col-md-10 col offset-md-1"><h5 class="title">${element.headline.main}</h5><p>${element.snippet}</p></div></div><br>`;
-        responseContainer.appendChild(noticia);
+        const noticia = `<div class="noticias container"><div class = "col-md-10 col offset-md-1"><h5 class="title">${element.headline.main}</h5><p>${element.snippet}</p></div></div><br>`;
+        responseContainer.innerHTML +=noticia;
       }
     });
   }
@@ -43,16 +42,16 @@ function addNews(response){
 
 // Para AJAX  con fetch
 btnFetch.addEventListener('click', function(e){
-   e.preventDefault();
-   responseContainer.innerHTML='';
-   searchedForText=searchField.value;
+  e.preventDefault();
+  responseContainer.innerHTML='';
+  searchedForText=searchField.value;
   getNews();
     let uri = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=67e2b2e80b4340dc9b9c27b472072dc7`;
-     fetch(uri)
-     .then(function(response){
+      fetch(uri)
+      .then(function(response){
         //  console.log(response);
-         return response.json();
-     }).then(function(data){
+          return response.json();
+      }).then(function(data){
         const article = data.response.docs[0];
         const title = article.headline.main;
         const snippet = article.snippet;
@@ -60,8 +59,8 @@ btnFetch.addEventListener('click', function(e){
         li.className = 'articleClass';
         li.innerText=snippet;
         responseContainer.appendChild(li);
-    })
-     .catch(function(error){
-         console.log(error);
-     });
+      })
+      .catch(function(error){
+        console.log(error);
+      });
 });
